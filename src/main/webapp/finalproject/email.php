19d25a36594ba108bc9bb52240952e95ac896280
mtb29 <bradymt29@mansfield.edu>
2017-04-20 07:52:23 -0400

    email validation

--- src/main/webapp/finalproject/email.php
+++ src/main/webapp/finalproject/email.php
@@ -6,10 +6,9 @@
         <?php
         $email = $_POST['email'];
         $results = $_POST['results'];
-        preg_match('\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', $email, $matches);
 
-        if (empty($email) || empty($matches[0])) {
-            echo "You have entered an invalid email address. Go back and try again.";
+        if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "You have entered an invalid email address. Go back and try again.<br/>";
         } else {
             mail($email, 'Quiz Results', $results);
             echo "The results have been sent to $email.<br/>";
