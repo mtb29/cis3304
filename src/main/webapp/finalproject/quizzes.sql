c2acd3d6eb7eb4b7ea0a4077a85b786f59acafa2
mtb29 <bradymt29@mansfield.edu>
2017-04-20 22:00:10 -0400

    added another quiz

--- src/main/webapp/finalproject/quizzes.sql
+++ src/main/webapp/finalproject/quizzes.sql
@@ -12,7 +12,7 @@ CREATE TABLE quiz_types (
     PRIMARY KEY (id)
 );
 
-INSERT INTO quiz_types (quiz_type) VALUES ('mySQL'), ('HTML'), ('PHP'), ('Math');
+INSERT INTO quiz_types (quiz_type) VALUES ('mySQL'), ('HTML'), ('PHP'), ('Math'), ('Puppies/Dogs');;
 
 DROP TABLE IF EXISTS quiz_questions;
 
@@ -43,4 +43,12 @@ INSERT INTO quiz_questions (quiz_type, question, a1, a2, a3, answer) VALUES
     (4, 'What is the answer to life, the universe, and everything?', '42', '69', '01001110 01000101 01010010 01000100 00100001', '42'),
     (4, 'A 20% tip on a $42.36 bill is?', '$8.47', '$0.00', '$9.47', '$8.47'),
     (4, 'How much interest is 12% APR on a $3600 loan after one year?', '$336', '$432', '$523', '$432'),
-    (4, 'In the equation: X + 1 - 1 = 4 - 1, what does X equal?', '3', '-1', '0', '3');
+    (4, 'In the equation: X + 1 - 1 = 4 - 1, what does X equal?', '3', '-1', '0', '3'),
+    (5, 'At what age are you able to take a puppy home?', '8 weeks', '9 weeks', '10 weeks', '8 weeks'),
+    (5, 'Who is mans best friend?', 'Cat', 'Zebra', 'Dog', 'Dog'),
+    (5, 'Of the three, what is the most popular dog name?', 'Fido', 'Ralph', 'George', 'Fido'),
+    (5, 'Dogs develop their sense of smell at what age?', '1 month', '3 weeks', '1 day', '3 weeks'),
+    (5, 'Of the three, which is an actual breed of dog?', 'ShihTzu', 'Kazuul', 'Snioooper', 'ShihTzu'),
+    (5, 'What dog breed is Lassie?', 'Golden Retriever', 'Collie', 'Australian Shepherd', 'Collie'),
+    (5, 'What dog breed is Scooby Doo?', 'Great Dane', 'Saint Bernard', 'Poodle', 'Great Dane'),
+    (5, 'In Scooby Doo 2 Monsters Unleashed, Scooby Doo comes to a conclusion, which is?', 'Yoinks!', 'Great Scott!', 'Bunny!', 'Bunny!');
